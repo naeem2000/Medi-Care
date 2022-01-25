5f09e422f4b78e8f81d9edbe34ea498936268a61
naeem2000 <mogamatnaeemcarr@gmail.com>
2022-01-25 11:04:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+(function() {
+    var $curve = document.getElementById("curve");
+    var last_known_scroll_position = 0;
+    var defaultCurveValue = 350;
+    var curveRate = 3;
+    var ticking = false;
+    var curveValue;
+
+    function scrollEvent(scrollPos) {
+      if (scrollPos >= 0 && scrollPos < defaultCurveValue) {
+        curveValue = defaultCurveValue - parseFloat(scrollPos / curveRate);
+        $curve.setAttribute(
+          "d",
+          "M 800 300 Q 400 " + curveValue + " 0 300 L 0 0 L 800 0 L 800 300 Z"
+        );
+      }
+    }
+  
+    window.addEventListener("scroll", function(e) {
+      last_known_scroll_position = window.scrollY;
+  
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          scrollEvent(last_known_scroll_position);
+          ticking = false;
+        });
+      }
+  
+      ticking = true;
+    });
+  })();
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+let map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+ 
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
